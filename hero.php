01336fe4affc2c56f1172dca0d213addc589dbea
configuserdir <malymoha@ies-eugeni.cat>
2024-02-13 17:52:51 +0100

    Plantilla_V1

--- hero.php
+++ hero.php
@@ -11,9 +11,9 @@
           <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
             <div class="carousel-container">
               <div class="carousel-content">
-                <h2 class="animate__animated animate__fadeInDown">Welcome to <span>Eterna</span></h2>
+                <h2 class="animate__animated animate__fadeInDown">Benvingut a <span>Pràctica 9</span></h2>
                 <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                <a href="" class="btn-get-started animate__animated animate__fadeInUp">Read More</a>
+                <a href="" class="btn-get-started animate__animated animate__fadeInUp">Llegeix mès ...</a>
               </div>
             </div>
           </div>
@@ -24,7 +24,7 @@
               <div class="carousel-content">
                 <h2 class="animate__animated fanimate__adeInDown">Lorem <span>Ipsum Dolor</span></h2>
                 <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                <a href="" class="btn-get-started animate__animated animate__fadeInUp">Read More</a>
+                <a href="" class="btn-get-started animate__animated animate__fadeInUp">Llegeix mès ...</a>
               </div>
             </div>
           </div>
@@ -35,7 +35,7 @@
               <div class="carousel-content">
                 <h2 class="animate__animated animate__fadeInDown">Sequi ea <span>Dime Lara</span></h2>
                 <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                <a href="" class="btn-get-started animate__animated animate__fadeInUp">Read More</a>
+                <a href="" class="btn-get-started animate__animated animate__fadeInUp">Llegeix mès ...</a>
               </div>
             </div>
           </div>
