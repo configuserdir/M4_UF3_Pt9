7db45c0932d98e9308095050096ec1b69e965c39
configuserdir <malymoha@ies-eugeni.cat>
2024-02-21 18:59:47 +0100

    HLABUENOS DIAS

--- header.php
+++ header.php
@@ -40,11 +40,44 @@
 
   <!-- ======= Top Bar ======= -->
   <section id="topbar" class="d-flex align-items-center">
+
     <div class="container d-flex justify-content-center justify-content-md-between">
       <div class="contact-info d-flex align-items-center">
         <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">info@eugeni.cat</a></i>
-        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+34 621 323 969</span></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+34 631 323 969</span></i>
       </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="portfolio.html">Portfolio</a></li>
+          <li><a class="active" href="team.html">Team</a></li>
+          <li><a href="pricing.html">Pricing</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
       <div class="social-links d-none d-md-flex align-items-center">
         <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
         <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
@@ -55,8 +88,6 @@
 
 
 
-
-    
   </section>
 
   <!-- ======= Header ======= -->
--- servei_actualitzar.php
+++ servei_actualitzar.php
@@ -2,27 +2,45 @@
 include("header.php");
 include("connexio_woo.php");
 
-$id=$_POST['id'];
-$name=$_POST['name'];
-$short_description=$_POST['short_description'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $short_description = $_POST['short_description'];
+    $price = $_POST['price'];
 
-$data = [
-    'name' => $name,
-    'price' => $price,
-    'short_description' => $short_description
-    
-];
+    $data = [
+        'name' => $name,
+        'price' => $price,
+        'short_description' => $short_description
+    ];
 
-($woocommerce->put('products/'.$id, $data));
+    $response = $woocommerce->put('products/'.$id, $data);
 
-function is_wp_error($thing) {}
+    if (!function_exists('is_wp_error')) {
+        $is_wp_error = function ($thing) {
+            return false;
+        };
+    } else {
+        $is_wp_error = 'is_wp_error';
+    }
 
-if (is_wp_error($response)) {
-    echo "Error en actualizar el producte: " . $response->get_error_message();
+    if (!function_exists('get_error_message')) {
+        $get_error_message = function ($thing) {
+            return '';
+        };
+    } else {
+        $get_error_message = 'get_error_message';
+    }
+
+    if ($is_wp_error($response)) {
+        echo "Error al actualitzar el producte: " . $get_error_message($response);
+    } else {
+        echo "El producte s'ha actualizat correctament.";
+    }
+
 } else {
-    echo "El producte s'ha actualitzat correctament.";
-}    
-
+    echo "<strong>Error</strong>: El formulari no s'ha enviat correctament.";
+}
 
 include('footer.php');
 ?>
