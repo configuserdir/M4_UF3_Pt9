1b50968a63fbf42cd5bd48b37e2b36804b22231d
configuserdir <malymoha@ies-eugeni.cat>
2024-02-26 15:48:43 +0100

    Co-authored-by: arnau-marti <arnau-marti@users.noreply.github.com>

--- header.php
+++ header.php
@@ -43,8 +43,8 @@
 
     <div class="container d-flex justify-content-center justify-content-md-between">
       <div class="contact-info d-flex align-items-center">
-        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">info@eugeni.cat</a></i>
-        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+34 631 323 969</span></i>
+        <i class="bi bi-envelope d-flex align-items-left"><a href="mailto:contact@example.com">info@eugeni.cat</a></i>
+        <i class="bi bi-phone d-flex align-items-left ms-4"><span>+34 631 323 969</span></i>
       </div>
 
       <nav id="navbar" class="navbar">
