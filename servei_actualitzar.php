24256a1c71a49e41801f07e967d117e215c3e209
Arnau Martí <usuari@domini.com>
2024-02-27 16:11:52 +0100

    servei esborrar

--- servei_actualitzar.php
+++ servei_actualitzar.php
@@ -10,8 +10,7 @@ foreach ($woocommerce->get('products') as $item) {
     // Verifica si la propiedad src_image existe en el objeto $item
     $src_image = property_exists($item, 'src_image') ? $item->src_image : '';
 
-    echo '<div class="col-md-1 form-group"><input class="form-control img-responsive" name="src_image" src="' . $src_image . '" style="" type="text"></div>';
-    echo '<div class="col-md-2 form-group"><input class="form-control" name="name" value="' . $item->name . '" style="width:100px;text-align:left;" type="text"></div>';
+    echo '<div class="col-md-4 form-group"><input class="form-control" name="name" value="' . $item->name . '" style="width:100px;text-align:left;" type="text"></div>';
     echo '<div class="col-md-4 form-group"><input class="form-control" name="short_description" value="' . strip_tags($item->short_description) . '" type="text"></div>';
     echo '<div class="col-md-1 form-group"><input class="form-control" name="price" value="' . strip_tags($item->price) . '" type="text"></div>';
     echo '<div class="col-md-1 form-group"><button class="btn center" formaction="servei_actualitzar_id.php" type="submit"><i class="bi bi-arrow-clockwise"></i></button></div>';
--- servei_esborrar.php
+++ servei_esborrar.php
@@ -1,8 +1,17 @@
 <?php include('header.php') ?>
 <?php include('connexio_woo.php') ?>
 <?php
-print_r($woocommerce->delete('products/id', ['force' => true]));
 
+if (isset($products) && (is_array($products) || is_object($products))) {
+    foreach ($products as $product) {
+        print_r($woocommerce->delete('products/id', ['force' => true]));
 
+        echo '<div class="col-md-2 form-group"><input class="form-control" name="name" value="' . $item->name . '" style="width:100px;text-align:left;" type="text"></div>';
+        echo '<div class="col-md-4 form-group"><input class="form-control" name="short_description" value="' . strip_tags($item->short_description) . '" type="text"></div>';
+        echo '<div class="col-md-1 form-group"><input class="form-control" name="price" value="' . strip_tags($item->price) . '" type="text"></div>';
+        echo '<div class="col-md-1 form-group"><button class="btn center" formaction="servei_esborrar.php" type="submit"><i class="bi bi-arrow-clockwise"></i></button></div>';
+        echo '</div></form>';
+    }
+}
 ?>
 <?php include('footer.php') ?>
